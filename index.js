3dea1ec4f85b890299eceb99f52946e6352155b9
Aaron Spurgeon <aspurgeon061@gmail.com>
2019-12-12 20:33:53 -0500

    Connected to the darksky api

--- index.js
+++ index.js
@@ -12,6 +12,44 @@ server.get("/", (req, res) => {
   });
 });
 
+server.get("/weather", (req, res) => {
+  const { lat, lon } = req.query;
+
+  let request = unirest(
+    "GET",
+    `https://${process.env.RAPID_HOST}/${lat},${lon}`
+  );
+
+  request.query({
+    lang: "en",
+    units: "auto"
+  });
+
+  request.end(res => {
+    if (res.error) res.status(500).end();
+
+    const {
+      summary,
+      precipProbability,
+      temperature,
+      windSpeed,
+      windBearing
+    } = res.body.currently;
+
+    res.status(200).send(
+      JSON.stringify({
+        summary: summary,
+        chanceOfRain: precipProbability,
+        temp: temperature,
+        wind: {
+          speed: windSpeed,
+          bearing: windBearing
+        }
+      })
+    );
+  });
+});
+
 const host = process.env.HOST || "0.0.0.0";
 const port = process.env.PORT || 8080;
 
