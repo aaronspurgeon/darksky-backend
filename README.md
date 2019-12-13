81cddb0e86d8b52c252aa8ac48992af530dbe30f
Aaron Spurgeon <aspurgeon061@gmail.com>
2019-12-12 20:18:39 -0500

    Have server running with a simple console.log saying a message

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a simple backend server in order to host the darksky API.
--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+const express = require("express");
+const cors = require("cors");
 
+const server = express();
+
+server.use(cors());
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Hello World"
+  });
+});
+
+const host = process.env.HOST || "0.0.0.0";
+const port = process.env.PORT || 8080;
+
+server.listen(port, host, () => {
+  console.log("app runnin");
+});
